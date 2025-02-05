6008d054b219c1e5265309daf10bf6cd8ae6e400
JunseoYun142857 <junseoyun0822@gmail.com>
2025-02-04 20:27:23 -0800

    Feat: Add Header with logo, menu, and user profile (#7)
    * header created
    
    * Update header component and fix logo issue
    
    * Update header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./global.css";
+import Header from "./components/header";
 
 import Login from "./pages/login";
 import Home from "./pages/home";
@@ -7,12 +8,16 @@ import Home from "./pages/home";
 function App() {
   return (
     <BrowserRouter>
+      <Header/>
+      <main style={{ paddingTop: "48px" }}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
       </Routes>
+      </main>
     </BrowserRouter>
   );
 }
 
 export default App;
+
