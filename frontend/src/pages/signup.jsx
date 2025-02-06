2e6c9be1b83a3f5b563224711f0aa8c2d7c04e58
Andrew Park <hep003@ucsd.edu>
2025-02-06 15:43:44 -0800

    Signup page for Frontend (#5)
    * init signin
    
    * add date picker
    
    * add states
    
    * add gender input
    
    * add visibility icon
    
    * handle black form
    
    * add password handler
    
    * init signin
    
    * add date picker
    
    * add visibility icon
    
    * handle black form
    
    * add password handler
    
    * edit in style
    
    * fix logic
    
    * fix logic 2
    
    * remove prints

--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -6,27 +6,52 @@ import {
   TextField,
   Link,
   Grid2 as Grid,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl,
 } from "@mui/material";
 import { motion } from "framer-motion";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
-export default function Login() {
-  const [showPassword, setShowPassword] = useState(false);
+export default function Signup() {
+  const [dob, setDob] = useState(new Date());
   const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [password, setPassword] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(email, password);
-  };
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [gender, setGender] = useState("");
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
   };
 
+  const handleFirstNameChange = (e) => {
+    setFirstName(e.target.value);
+  };
+
+  const handleLastNameChange = (e) => {
+    setLastName(e.target.value);
+  };
+
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+  };
+
+  // TODO: Create a function to handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
   };
 
   return (
@@ -66,7 +91,7 @@ export default function Login() {
             background:
               "linear-gradient(to bottom, rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0))",
 
-            top: "55%",
+            top: "50%",
             transform: "translateY(-50%)",
             left: "2%",
             zIndex: 1,
@@ -79,7 +104,7 @@ export default function Login() {
           transition={{ duration: 0.8 }}
         >
           <Typography variant="h2" sx={{ color: "white", fontWeight: 700 }}>
-            Roomiverse
+            Welcome to Roomiverse!
           </Typography>
         </motion.div>
         <motion.div
@@ -121,7 +146,7 @@ export default function Login() {
         }}
       >
         <Typography variant="h3" sx={{ fontWeight: 700, color: "#283b42" }}>
-          Login
+          Signup
         </Typography>
         <Box
           variant="form"
@@ -132,6 +157,30 @@ export default function Login() {
             spacing={2}
             sx={{ width: "100%", display: "flex", flexDirection: "column" }}
           >
+            <Grid
+              item
+              xs={12}
+              sx={{ display: "flex", flexDirection: "row", gap: "1rem" }}
+            >
+              <TextField
+                required
+                fullWidth
+                label="First Name"
+                variant="outlined"
+                color="black"
+                onChange={handleFirstNameChange}
+                sx={{ color: "black" }}
+              />
+              <TextField
+                required
+                fullWidth
+                label="Last Name"
+                variant="outlined"
+                color="black"
+                onChange={handleLastNameChange}
+                sx={{ color: "black" }}
+              />
+            </Grid>
             <Grid item xs={12}>
               <TextField
                 fullWidth
@@ -151,18 +200,20 @@ export default function Login() {
               <TextField
                 fullWidth
                 required
-                type={showPassword ? "text" : "password"}
                 label="Password"
                 variant="outlined"
+                type={showPassword ? "text" : "password"}
                 color="black"
-                sx={{ marginBottom: 1, marginTop: 1, color: "black" }}
                 onChange={handlePasswordChange}
+                sx={{ color: "black" }}
                 slotProps={{
                   input: {
                     endAdornment: showPassword ? (
                       <VisibilityIcon
                         onClick={() => setShowPassword(!showPassword)}
-                        sx={{ "&:hover": { cursor: "pointer" } }}
+                        sx={{
+                          "&:hover": { cursor: "pointer" },
+                        }}
                       />
                     ) : (
                       <VisibilityOffIcon
@@ -173,29 +224,75 @@ export default function Login() {
                   },
                 }}
               />
-              <Link
-                href="/forgot-password"
-                underline="hover"
-                sx={{
-                  display: "display",
-                  marginTop: 1,
-                  fontSize: "0.9rem",
-                  alignSelf: "flex-end",
-                  "&:hover": {
-                    cursor: "pointer",
-                    textDecoration: "underline",
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              sx={{ display: "flex", flexDirection: "column" }}
+            >
+              <TextField
+                fullWidth
+                required
+                label="Confirm Password"
+                variant="outlined"
+                color="black"
+                type={showConfirmPassword ? "text" : "password"}
+                sx={{ color: "black" }}
+                onChange={handleConfirmPasswordChange}
+                slotProps={{
+                  input: {
+                    endAdornment: showConfirmPassword ? (
+                      <VisibilityIcon
+                        onClick={() =>
+                          setShowConfirmPassword(!showConfirmPassword)
+                        }
+                        sx={{ "&:hover": { cursor: "pointer" } }}
+                      />
+                    ) : (
+                      <VisibilityOffIcon
+                        onClick={() =>
+                          setShowConfirmPassword(!showConfirmPassword)
+                        }
+                        sx={{ "&:hover": { cursor: "pointer" } }}
+                      />
+                    ),
                   },
                 }}
-              >
-                <Typography>Forgot password</Typography>
-              </Link>
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DatePicker
+                  label="Date of Birth"
+                  onChange={(newValue) => setDob(newValue)}
+                  slotProps={{
+                    textField: { fullWidth: true },
+                  }}
+                />
+              </LocalizationProvider>
+            </Grid>
+            <Grid item sx={12}>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-standard-label">
+                  Gender
+                </InputLabel>
+
+                <Select
+                  fullWidth
+                  labelId="demo-simple-select-standard-label"
+                  label="Gender"
+                  onChange={(e) => setGender(e.target.value)}
+                >
+                  <MenuItem value={"Male"}>Male</MenuItem>
+                  <MenuItem value={"Female"}>Female</MenuItem>
+                </Select>
+              </FormControl>
             </Grid>
             <Grid item xs={12}>
               <Button
                 fullWidth
                 type="submit"
                 variant="contained"
-                onClick={handleSubmit}
                 sx={{
                   backgroundColor: "#FF6F61",
                   color: "white",
@@ -203,12 +300,21 @@ export default function Login() {
                   borderRadius: "0.5rem",
                   py: "0.75rem",
                 }}
-                disabled={!email || !password}
+                onClick={handleSubmit}
+                disabled={
+                  !firstName ||
+                  !lastName ||
+                  !email ||
+                  !password ||
+                  !confirmPassword ||
+                  !dob ||
+                  !gender
+                }
               >
-                <Typography sx={{ fontSize: 24 }}>Sign In</Typography>
+                <Typography sx={{ fontSize: 24 }}>Sign Up</Typography>
               </Button>
               <Box sx={{ display: "flex", marginTop: 1, gap: 1 }}>
-                <Typography>Don&apos;t have an account?</Typography>
+                <Typography>Already have an account?</Typography>
                 <Link
                   sx={{
                     textDecoration: "none",
@@ -217,9 +323,9 @@ export default function Login() {
                       textDecoration: "underline",
                     },
                   }}
-                  href="/signup"
+                  href="/login"
                 >
-                  <Typography>Signup</Typography>
+                  <Typography>Login</Typography>
                 </Link>
               </Box>
             </Grid>
