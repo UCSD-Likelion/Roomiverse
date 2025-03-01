25b68354e08d28a8ffac2af31e9c91e5c9d58a29
spark-park12 <146790038+spark-park12@users.noreply.github.com>
2025-02-28 16:24:51 -0800

    hiTv

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,10 @@ import Header from "./components/header";
 import LandingPage from "./pages/landing";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
+import Dashboard from "./pages/dashboard";
+import PrivateRoute from "./utils/private-route";
+import MatchingForm from "./pages/matchingForm";
+import Profile from "./pages/profile";
 
 function App() {
   return (
@@ -13,7 +17,12 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/register" element={<Signup />} />
+          <Route path="/" element={<PrivateRoute />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
+          <Route path="/matching-form" element={<MatchingForm />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </main>
     </BrowserRouter>
