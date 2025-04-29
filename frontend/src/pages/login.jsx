32df013dfe2289773fc9b34888d661971b54b6be
7174Andy <hep003@ucsd.edu>
2025-04-28 17:30:47 -0700

    refactor login

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -4,22 +4,15 @@ import {
   Alert,
   Box,
   Typography,
-  Button,
-  TextField,
-  Link,
   Snackbar,
-  Grid2 as Grid,
   Card,
   CardContent,
-  IconButton,
-  InputAdornment,
 } from "@mui/material";
 import { motion } from "framer-motion";
-import VisibilityIcon from "@mui/icons-material/Visibility";
-import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
 import { loginValidation } from "../utils/validators";
 import { AuthContext } from "../context/AuthProvider";
+import LoginForm from "../components/login-form";
 
 export default function Login() {
   const location = useLocation();
@@ -135,130 +128,16 @@ export default function Login() {
             Login
           </Typography>
 
-          <Grid
-            container
-            spacing={2}
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-            }}
-          >
-            <Grid item xs={6}>
-              <TextField
-                fullWidth
-                label="Email"
-                variant="filled"
-                size="small"
-                onChange={handleEmailChange}
-                error={!!error.email}
-                helperText={error.email}
-                value={email}
-                slotProps={{
-                  input: {
-                    sx: {
-                      bgcolor: "#E6E9FF",
-                      borderRadius: 2,
-                      pl: 1,
-                      fontSize: "16px",
-                    },
-                    disableUnderline: true,
-                  },
-                  inputLabel: {
-                    shrink: true,
-                    sx: {
-                      fontWeight: 600,
-                      fontSize: "14px",
-                      color: "text.primary",
-                      mb: 0.5,
-                    },
-                  },
-                }}
-                sx={{
-                  mt: 1,
-                }}
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <TextField
-                fullWidth
-                label="Password"
-                variant="filled"
-                onChange={handlePasswordChange}
-                error={!!error.password}
-                helperText={error.password}
-                value={password}
-                size="small"
-                type={showPassword ? "text" : "password"}
-                slotProps={{
-                  input: {
-                    sx: {
-                      bgcolor: "#E6E9FF",
-                      borderRadius: 2,
-                      pl: 1,
-                      fontSize: "16px",
-                    },
-                    disableUnderline: true,
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <IconButton
-                          onClick={() => setShowPassword(!showPassword)}
-                          edge="end"
-                          size="small"
-                        >
-                          {showPassword ? (
-                            <VisibilityIcon />
-                          ) : (
-                            <VisibilityOffIcon />
-                          )}
-                        </IconButton>
-                      </InputAdornment>
-                    ),
-                  },
-                  inputLabel: {
-                    shrink: true,
-                    sx: {
-                      fontWeight: 600,
-                      fontSize: "14px",
-                      color: "text.primary",
-                      mb: 0.5,
-                    },
-                  },
-                }}
-                sx={{
-                  mt: 1,
-                }}
-              />
-            </Grid>
-          </Grid>
-
-          <Box mt={3} textAlign="center">
-            <Button
-              variant="contained"
-              size="large"
-              onClick={handleSubmit}
-              sx={{
-                borderRadius: "30px",
-                px: 3,
-                width: "50%",
-                backgroundColor: "#A9B8FF",
-                color: "#fff",
-                ":hover": {
-                  backgroundColor: "#91A3E0",
-                },
-              }}
-            >
-              Login
-            </Button>
-          </Box>
-
-          <Box mt={2} textAlign="center">
-            <Typography variant="body2">
-              Don't have an account{" "}
-              <Link href="/signup" underline="hover">
-                Sign up
-              </Link>
-            </Typography>
-          </Box>
+          <LoginForm
+            email={email}
+            password={password}
+            showPassword={showPassword}
+            error={error}
+            onEmailChange={handleEmailChange}
+            onPasswordChange={handlePasswordChange}
+            onShowPasswordToggle={() => setShowPassword(!showPassword)}
+            onSubmit={handleSubmit}
+          />
         </CardContent>
       </Card>
       <Snackbar
