f60fa020b64d88db623d6c659eea24c7872f9456
7174Andy <hep003@ucsd.edu>
2025-05-05 17:42:09 -0700

    update loading page

--- frontend/src/utils/private-route.jsx
+++ frontend/src/utils/private-route.jsx
@@ -1,12 +1,26 @@
 import { useContext } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { AuthContext } from "../context/AuthProvider";
+import { Box, CircularProgress, Typography } from "@mui/material";
 
 const PrivateRoute = () => {
   const { user, loading } = useContext(AuthContext);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="100vh"
+        flexDirection="column"
+      >
+        <CircularProgress />
+        <Typography variant="body1" mt={2}>
+          Checking authentication...
+        </Typography>
+      </Box>
+    );
   }
 
   return user ? <Outlet /> : <Navigate to="/login" />;
