2903d0bb84b2fc94cfc407c4f4e89f309f303aab
Andrew Park <hep003@ucsd.edu>
2025-05-28 20:31:28 -0700

    finish form (#38)

--- frontend/src/pages/matchingForm.jsx
+++ frontend/src/pages/matchingForm.jsx
@@ -169,8 +169,8 @@ export default function MatchingForm() {
 
         {currentPage === 4 && (
           <MatchingForm5
-            ethnicity={ethnicity}
-            handleEthnicityChange={handleEthnicityChange}
+            roomType={roomType}
+            handleRoomTypeChange={handleRoomTypeChange}
             distance={distance}
             handleDistanceChange={handleDistanceChange}
             setCurrentPage={setCurrentPage}
